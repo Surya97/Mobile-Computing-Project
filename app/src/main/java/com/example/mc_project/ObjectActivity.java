19a75e4c9f7378541d089b0b9822d9c44c6b2c6d
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-11-26 01:58:40 -0700

    Fixed Camera image retrieval issue and object classification is working. Issues left - TTS not working.

--- app/src/main/java/com/example/mc_project/ObjectActivity.java
+++ app/src/main/java/com/example/mc_project/ObjectActivity.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -49,29 +50,38 @@ public class ObjectActivity extends AppCompatActivity {
         }
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     200);
         }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    200);
+        }
+
+    }
+    public void startCapture(View view){
         Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(camera, PIC_REQUEST_ID);
-
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if (requestCode == PIC_REQUEST_ID){
             if(resultCode == RESULT_OK) {
-                Bitmap photo = (Bitmap) data.getExtras().get("data");
-                imageUri = getImageUri(getApplicationContext(), photo);
-                if(imageUri !=null){
+                if(data!=null){
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    imageUri = getImageUri(getApplicationContext(), photo);
+                    if(imageUri !=null){
 
+                    }
+                    Log.d("Image URI: ", imageUri.toString());
+                    File imageFile = new File(getRealPathFromURI(imageUri));
+
+                    ObjectClassify classification = new ObjectClassify();
+                    classification.execute(imageFile);
                 }
-                Log.d("Image URI: ", imageUri.toString());
-                File imageFile = new File(getRealPathFromURI(imageUri));
+                Toast.makeText(ObjectActivity.this, "Data response is null", Toast.LENGTH_LONG);
 
-                ObjectClassify classification = new ObjectClassify();
-                String objectClass = classification.classify(imageFile);
-                TTS tts = new TTS();
-                tts.textToSpeech("The object is " + objectClass);
             }
         }
     }
@@ -95,9 +105,9 @@ public class ObjectActivity extends AppCompatActivity {
         return path;
     }
 
-    private class ObjectClassify{
+    private class ObjectClassify extends AsyncTask<File, String, String> {
         public String classify(File imageFile){
-            IamAuthenticator authenticator = new IamAuthenticator("key");
+            IamAuthenticator authenticator = new IamAuthenticator("GMEIU-KCqpTuqU4on2pRU5eLiWCLRHsp-emjHHA9wlbS");
             VisualRecognition visualRecognition = new VisualRecognition("2018-03-19", authenticator);
             visualRecognition.setServiceUrl("https://gateway.watsonplatform.net/visual-recognition/api");
             String output="";
@@ -117,6 +127,18 @@ public class ObjectActivity extends AppCompatActivity {
             return output;
         }
 
+        @Override
+        protected String doInBackground(File... files) {
+            return classify(files[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String objectClass){
+            TTS tts = new TTS();
+            Toast.makeText(ObjectActivity.this, "Object TTS "+ objectClass, Toast.LENGTH_LONG);
+            Log.d("Object Class", objectClass);
+            tts.textToSpeech("The object is " + objectClass);
+        }
     }
 
     private class TTS{
